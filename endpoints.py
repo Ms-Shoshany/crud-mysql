1ab2c73a0619fe35c7b2cc4e2ae09045136037b5
hanna <channashosh@gmail.com>
2025-07-11 14:27:12 +0300

    error managment

--- endpoints.py
+++ endpoints.py
@@ -7,7 +7,7 @@ object_page = Blueprint('object_page', __name__, template_folder='templates')
 @object_page.route("/<object_type>", methods=["GET"])
 def get_object(object_type):
 	if not object.check_object(object_type):
-		return jsonify(f"Invalid path: /{object_type}"), 400
+		return jsonify(f"Invalid path: /{object_type}"), 404
 	object_id = request.args.get(f"{object_type}_id")
 	if not object_id:
 		return(f"no {object_type} id was provided. (add {object_type}_id argument)"), 400
@@ -23,8 +23,10 @@ def get_object(object_type):
 @object_page.route("/<object_type>", methods=["POST"])
 def create_object(object_type):
 	if not object.check_object(object_type):
-		return jsonify(f"Invalid path: /{object_type}"), 400
+		return jsonify(f"Invalid path: /{object_type}"), 404
 	data = request.json
+	if not object_type in data:
+		return jsonify(f"missing {object_type} info. (add {object_type} object in body)"), 400
 	object_info = data[object_type]
 	object_id = object.create_object(object_type, object_info)
 	if object_id:
@@ -35,9 +37,11 @@ def create_object(object_type):
 @object_page.route("/<object_type>", methods=["PUT"])
 def update_object(object_type):
 	if not object.check_object(object_type):
-		return jsonify(f"Invalid path: /{object_type}"), 400
+		return jsonify(f"Invalid path: /{object_type}"), 404
 	object_id = request.args.get(f"{object_type}_id")
 	data = request.json
+	if not object_type in data:
+		return jsonify(f"missing {object_type} info to change. (add {object_type} object in body)"), 400
 	object_info = data[object_type]
 	object.update_object(object_type, object_id, object_info)
 	return jsonify(f"Successfully updated {object_type} with id: {object_id}"), 200
@@ -45,7 +49,7 @@ def update_object(object_type):
 @object_page.route("/<object_type>", methods=["DELETE"])
 def delete_object(object_type):
 	if not object.check_object(object_type):
-		return jsonify(f"Invalid path: /{object_type}"), 400
+		return jsonify(f"Invalid path: /{object_type}"), 404
 	object_id = request.args.get(f"{object_type}_id")
 	object.delete_object(object_type, object_id)
 	return jsonify(f"Successfully deleted {object_type} with id: {object_id}"), 204
